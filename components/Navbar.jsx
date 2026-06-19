2a06f5bb41a070390b31270bab0fd5709620cde7
The-Manish09 <manishchauhan22.9mc@gmail.com>
2026-06-19 14:47:58 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+   <header className='header'>
+    <NavLink to='/' className="w-10 h-10 rounded-lg bg-whitw items-center justify-center flex font-bold shadow-md">
+    <p className='blue-gradient_text'>MC</p>
+    </NavLink>
+    <nav className=''>
+        <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'} >
+        About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+        Projects
+        </NavLink>
+    </nav>
+   </header>
+  )
+}
+
+export default Navbar
